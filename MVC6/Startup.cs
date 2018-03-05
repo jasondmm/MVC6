54443e94d0bec3b14b715b249168752885f13a89
JDMM <jasondmm@gmail.com>
2018-03-05 00:38:35 -0600

    Inicio

---
+++ MVC6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC6.Startup))]
+namespace MVC6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
